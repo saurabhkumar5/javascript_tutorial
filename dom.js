ee47259b4b3db0b23146a0d6c77e2af126c7027c
saurabh kumar <saurabhkumar9493@gmail.com>
2023-12-21 17:35:06 +0530

    dom

---
+++ dom.js
@@ -0,0 +1,21 @@
+// console.log("hello")
+// document.write("<h2>hello</h2>")
+
+// console.log(window.document)
+
+// 1 by id
+// let a = document.getElementById("hello")
+// console.log(a);
+
+// //2. by class
+//     let b = document.getElementsByClassName("world")
+//     console.log(b);
+
+// // 3. by tagname
+//        let c = document.getElementsByTagName("p")
+//        console.log(c);
+//4  by queryselector
+
+//   let a = document.querySelector(".world")
+let a = document.querySelectorAll("#world")
+  console.log(a)
