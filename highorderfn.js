744d016d04c901423b1ae3966982cae29531e339
saurabh kumar <saurabhkumar9493@gmail.com>
2023-12-29 21:33:51 +0530

    js

--- highorderfn.js
+++ highorderfn.js
@@ -44,12 +44,121 @@
 
 // 4. forEach()
 
-let arr = [4,78,56,10]
+// let arr = [4,78,56,10]
 
-   arr.forEach((a,b)=>{
-         if(a%3==0){
-            console.log(a)
-         }
-    })
+//    arr.forEach((a,b)=>{
+//          if(a%3==0){
+//             console.log(a)
+//          }
+//     })
 
-    
+
+
+// reduce function
+
+// let arr = [12,23,1,4]
+
+//   let sum = arr.reduce((a,b)=>{
+//     return a+b
+//   })
+
+//   console.log(sum)
+
+
+
+//setTimeout function
+
+  
+    // console.log("fist")
+    // setTimeout(()=>{
+    //   console.log("hello")
+    // },5000)
+
+    // console.log("second")
+
+
+    // setInterval(()=>{
+    //   console.log("hello")
+    // },2000)
+
+
+//default parameter
+
+// function sum(a,b=2){
+//   console.log(a+b)
+// }
+
+
+//   sum(1,5)
+
+
+
+// spread operator
+
+// let a = "saurabh"
+// let arr = [12345]
+// console.log(...arr);
+
+
+// let even = [2,4,6,8]
+// let odd = [1,3,5,7]
+
+// let arr = [...even,...odd]
+// console.log(arr)
+
+
+// spread with objects
+
+// let obj = {
+//   name:"ajit",
+//   age:45,
+//   class:"js"
+// }
+
+
+// let obj1 = {
+//   name1:"ajit",
+//   age1:45,
+//   class1:"js"
+// }
+
+// let a = {...obj,...obj1}
+// console.log(a)
+
+
+
+// Rest operator 
+
+  // function hello(...a){
+         
+  //   console.log(a);
+  // }
+
+  // hello(2,4,3,8)
+
+
+
+  // destructuring
+
+
+  // let arr = ["ajit","aakash","anuj","kaajal"]
+
+//  let a = arr[0]
+//  let b = arr[1];
+//  console.log(a,b)
+
+// let [a,b,c] = arr;
+
+// console.log(a,b,c);
+
+
+let obj = {
+  name:"ajit",
+  age:45,
+  subj:"js"
+}
+
+
+let {name,age,subj} = obj;
+
+console.log(age)
